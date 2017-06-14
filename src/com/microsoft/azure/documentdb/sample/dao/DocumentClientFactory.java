4a1851f281b04631e5b119580b5ed5d0cfe0e045
anuragagrawal1 <anuragagrawal1@gmail.com>
2017-06-14 11:10:23 -0700

    Create DocumentClientFactory.java

--- src/com/microsoft/azure/documentdb/sample/dao/DocumentClientFactory.java
+++ src/com/microsoft/azure/documentdb/sample/dao/DocumentClientFactory.java
@@ -5,8 +5,9 @@ import com.microsoft.azure.documentdb.ConsistencyLevel;
 import com.microsoft.azure.documentdb.DocumentClient;
 
 public class DocumentClientFactory {
-    private static final String HOST = "https://docdb-java-sample.documents.azure.com:443/";
-    private static final String MASTER_KEY = "[YOUR_KEY_HERE]";
+    
+    private static final String HOST = "https://aacomosdb.documents.azure.com:443/";
+    private static final String MASTER_KEY = "VlGZRbeGvnrcJVK5shbpaRTiFl6k3HwMYYbojYO4FE5G0IU7VcnlFPRUxrSM4hKFLXsKnjsHFj0s2tvveViaaw==";
 
     private static DocumentClient documentClient = new DocumentClient(HOST, MASTER_KEY,
             ConnectionPolicy.GetDefault(), ConsistencyLevel.Session);
